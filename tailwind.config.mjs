aed9ec6fcbf7273f78001f80d0494b171abad122
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-06 06:53:06 -0500

    added @konj-org/preact-ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,84 @@
+// tailwind.config.js
+import colors from "tailwindcss/colors";
+import containerQueries from "@tailwindcss/container-queries";
+
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+const config = {
+  content: [
+    "node_modules/@konj-org/preact-ui/**/*.tsx",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    transitionTimingFunction: {
+      elastic: "cubic-bezier(0.68, 0, 0.32, 1.2)",
+    },
+    fontFamily: {
+      sans: [
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Open Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+      mono: [
+        "SF Mono",
+        "SFMono-Regular",
+        "ui-monospace",
+        "DejaVu Sans Mono",
+        "Menlo",
+        "Consolas",
+        "monospace",
+      ],
+    },
+    extend: {
+      keyframes: {
+        "opacity-keyframes": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        "loading-keyframes": {
+          from: {
+            opacity: 0.5,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        "fade-in": "opacity-keyframes .5s ease-in-out forwards",
+        loading: "loading-keyframes infinite",
+      },
+      colors: {
+        primary: colors.rose,
+      },
+      screens: {
+        standalone: {
+          raw: "(display-mode: standalone)",
+        },
+        "iphone-portrait": {
+          raw: `
+            (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
+            (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
+            (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
+            (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
+            (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)
+          `,
+        },
+      },
+    },
+  },
+  plugins: [containerQueries],
+};
+
+export default config;
