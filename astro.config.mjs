27c65a66d84ba5cac29e9c5e52b2651fc353140b
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-09 07:33:05 -0500

    added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+// Plugins
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [
+    preact(),
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "nord" },
+      remarkRehype: { footnoteLabel: "Footnotes" },
+      gfm: false,
+    }),
+  ],
 });
