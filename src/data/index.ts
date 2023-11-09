28059c886cb0df04503bce504fd64d694b796431
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-09 03:05:04 -0500

    Updated landing page hero section

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,11 @@ type MahtabData = {
   title: string;
   description: string;
   websiteURL: string;
+  socials: {
+    name: string;
+    icon: string;
+    href: string;
+  }[];
 };
 
 import Data from "./data.json";
