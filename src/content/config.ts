9c2d564bd49e8affa484a684396c527131195670
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-10 07:02:02 -0500

    added projects to content

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { z, defineCollection } from "astro:content";
+
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    type: z.string(),
+    role: z.string(),
+    description: z.string(),
+    // Assets
+    banner: z.string(),
+    // Links
+    website: z.string().optional(),
+    repository: z.string().optional(),
+    // Dates
+    startDate: z.date().optional(),
+    endDate: z.date().optional(),
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+};
