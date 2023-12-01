58a73eaf27433ec8b43424e268ce6ac6a6082461
Amir <60965542+amir4rab@users.noreply.github.com>
2023-12-01 10:05:57 -0500

    added blog content

---
+++ src/content/blogs/starting-guide.md
@@ -0,0 +1,16 @@
+---
+title: Staring Guide
+description: A short guide on how to get started with mahtab
+date: 29-11-2023
+tags:
+  - mahtab
+  - astro
+  - guide
+state: published
+banner: getting-started-guide-v-0-1.jpg
+bannerRatio: 16/9
+website: https://mahtab.amir4rab.com/
+repository: https://github.com/amir4rab/mahtab
+---
+
+TO BE COMPLETED...
--- src/content/config.ts
+++ src/content/config.ts
@@ -40,7 +40,29 @@ const experiencesCollection = defineCollection({
   }),
 });
 
+const blogsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    tags: z.array(z.string()),
+    state: z.enum([
+      "published", // Will be listed on the blogs page
+      "draft", // Won't be listed on the blogs page, but would be visible with the url
+      "private", // Won't be rendered
+    ]),
+    // Assets
+    banner: z.string(),
+    bannerRatio: z.string(),
+    // Links
+    website: z.string().optional(),
+    repository: z.string().optional(),
+  }),
+});
+
 export const collections = {
   projects: projectsCollection,
   experiences: experiencesCollection,
+  blogs: blogsCollection,
 };
